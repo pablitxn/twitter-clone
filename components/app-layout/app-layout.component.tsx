7c0d316bacedf3d287923e40b4ef6f342855efb0
Pablo Coronel <me@pablitxn.io>
2020-08-23 00:31:09 -0300

    feat(styles): se configuro sass en la app

--- components/app-layout/app-layout.component.tsx
+++ components/app-layout/app-layout.component.tsx
@@ -1,23 +1,17 @@
+// Styles
+import "./app-layout.styles.scss";
+
 interface IAppLayout {
 	children: React.ReactNode;
 }
-
-export default function AppLayout({ children }: IAppLayout) {
+const AppLayout = ({ children }) => {
 	return (
 		<>
-			<main>{children}</main>
-			<style jsx global>{`
-				html,
-				body {
-					padding: 0;
-					margin: 0;
-					font-family: monospace;
-				}
-
-				* {
-					box-sizing: border-box;
-				}
-			`}</style>
+			<div className="app-layout">
+				<main className="main">{children}</main>
+			</div>
 		</>
 	);
-}
+};
+
+export default AppLayout;
